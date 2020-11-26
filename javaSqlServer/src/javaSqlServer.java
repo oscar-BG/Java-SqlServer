19b4368acd6ea8bfce0d760ff1eb04ac1a0212db
Oscar Bautista <oscar04262000@gmail.com>
2020-11-25 20:05:23 -0600

    Empezamos con el proyecto de JAVA creando una conexion con SQL server

---
+++ javaSqlServer/src/javaSqlServer.java
@@ -0,0 +1,9 @@
+
+public class javaSqlServer {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
